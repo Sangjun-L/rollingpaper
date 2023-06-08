38a5fdea91b460ef11b274694e5cac43a13a586f
진민지 <67037321+realmingji@users.noreply.github.com>
2023-06-09 00:54:07 +0900

    patch : 기능 수정

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-
 load_dotenv()
 MONGODB_URL = os.getenv('MONGODB_URL')
 
@@ -13,23 +12,18 @@ ca = certifi.where()
 client = MongoClient(MONGODB_URL, tlsCAFile=ca)
 db = client.rollingsparta
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/member/<member_id>')
 def member(member_id):
     return render_template('member.html')
 
-
 @app.route('/api/team', methods=["GET"])
 def member_list():
-    all_member = list(db.member.find({}, {'_id': False, 'mbti': False, 'musicURL': False,
-                                          'location': False, 'location': False, 'hobby': False}).sort('name', 1))
+    all_member = list(db.member.find({}, {'_id': False, 'mbti': False, 'musicURL': False,'location': False, 'hobby': False}).sort('name', 1))
     return jsonify({'result': 'success', 'list': all_member})
-
 
 @app.route('/api/member/<member_id>', methods=["GET"])
 def get_member(member_id):
@@ -38,13 +32,12 @@ def get_member(member_id):
         return jsonify({'result': 'fail', 'message': '없는 팀원인데요!'})
     return jsonify({'result': 'success', 'data': member_info})
 
-
-@app.route('/api/rollingpaper/<member_id>', methods=["POST"])
-def make_rolling(member_id):
+@app.route('/api/rollingpaper', methods=["POST"])
+def make_rolling():
     rolling_list = list(db.comment.find({}, {'_id': False}))
     count = len(rolling_list) + 1
     doc = {
-        'memberId': member_id,
+        'memberId': request.form['memberId'],
         'commentId': count,
         'name': request.form['name'],
         'password': request.form['password'],
@@ -54,18 +47,13 @@ def make_rolling(member_id):
     db.comment.insert_one(doc)
     return jsonify({'result': 'success', 'message': '등록이 완료되었습니다.'})
 
-
 @app.route('/api/rollingpaper/<member_id>', methods=["GET"])
 def get_rolling(member_id):
-    rolling_list = list(db.comment.find({'memberId': member_id}, {
-                        '_id': False, 'memberId': False, 'password': False}).sort('date', -1))
+    rolling_list = list(db.comment.find({'memberId': member_id}, {'_id': False, 'memberId': False, 'password': False}).sort('date', -1))
     return jsonify({'result': 'success', 'list': rolling_list})
 
-# 페이퍼 삭제
-
-
-@app.route('/api/rollingpaper', methods=["DELETE"])
-def del_rolling():
+@app.route('/api/rollingpaper/<member_id>', methods=["DELETE"])
+def del_rolling(member_id):
     comment_id = request.form['commentId']
     input_password = request.form['password']
     comment = db.comment.find_one({"commentId": int(comment_id)})
@@ -75,7 +63,6 @@ def del_rolling():
 
     db.comment.delete_one({"commentId": int(comment_id)})
     return jsonify({'result': 'success', 'message': '삭제 완료되었습니다.'})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/ggamchingMode.js
+++ static/js/ggamchingMode.js
@@ -1,13 +1,13 @@
 function setIsGgamchingMode() {
-  localStorage.getItem('gammchingMode') === 'on'
-    ? localStorage.setItem('gammchingMode', 'off')
-    : localStorage.setItem('gammchingMode', 'on');
+  sessionStorage.getItem("gammchingMode") === "on"
+    ? sessionStorage.setItem("gammchingMode", "off")
+    : sessionStorage.setItem("gammchingMode", "on");
   isGgamchingMode();
 }
 
 function isGgamchingMode() {
   const body = $('body');
-  localStorage.getItem('gammchingMode') === 'on'
-    ? body.addClass('dalcongcongMode')
-    : body.removeClass('dalcongcongMode');
+  sessionStorage.getItem("gammchingMode") === "on"
+    ? body.addClass("dalcongcongMode")
+    : body.removeClass("dalcongcongMode");
 }
