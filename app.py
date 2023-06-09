08fd4532035a5ba2f38722c4632e9f3208cc4c80
dgyoon <douglasyoon9@gmail.com>
2023-06-09 15:50:13 +0900

    fix : 방명록 무한 스크롤 API 요청 문제 해결

--- app.py
+++ app.py
@@ -59,12 +59,12 @@ def make_rolling(member_id):
 def get_rolling(member_id):
     page = request.args.get('page', 1, type=int)
     limit = 9
+    tot_count = db.comment.count_documents({})
+    last_page_num = math.ceil(tot_count / limit) - 1
     if page == 1:
         limit -= 1
     rolling_list = list(db.comment.find({'memberId': member_id}, {
                         '_id': False, 'memberId': False, 'password': False}).sort('date', -1).skip((page - 1) * limit).limit(limit))
-    tot_count = db.comment.count_documents({})
-    last_page_num = math.ceil(tot_count / limit)
     return jsonify({'result': 'success', 'list': rolling_list, 'limit': limit, 'page': page, 'last_page': last_page_num})
 
 
